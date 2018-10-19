d673f99037d75998da7056606de0f464588f4cbf
Charlotte Koch <cfkoch@edgebsd.org>
2018-10-19 16:31:43 -0700

    + temporary __charlotte.sh

---
+++ code/__charlotte.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+set -ex
+
+mkdir -p __build
+
+(
+  cd __build && cmake \
+    -DWITH_NEL=TRUE \
+    -DWITH_NEL_TESTS=FALSE \
+    -DWITH_SOUND=FALSE \
+    -DWITH_RYZOM_CLIENT=FALSE \
+    -DWITH_GUI=FALSE \
+    ..
+)
+
+make -C __build -j4 
+
+    #-DWITH_3D=FALSE \
+    #-DWITH_NEL_SAMPLES=FALSE \
+
+# PATCHES IN
+# - code/nel/include/nel/misc/types_nl.h
+# - code/nel/src/misc/system_info.cpp
+# - code/nel/src/misc/CMakeLists.txt
+# - code/ryzom/tools/stats_scan/character_scan_job.cpp
+#
+# - code/nel/samples/net/udp/CMakeLists.txt <-- not necessary if
+# WITH_NEL_SAMPLES=FALSE
+# -
